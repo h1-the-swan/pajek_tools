33c4c4d1a6c7f2993b54fb4472a44bef62d53999
hi-the-swan <jason.portenoy@gmail.com>
2020-02-13 08:19:38 -0800

    plan pajek save using pandas

--- pajek_tools/pajek_tools.py
+++ pajek_tools/pajek_tools.py
@@ -1,1 +1,41 @@
-"""Main module."""
+# -*- coding: utf-8 -*-
+
+DESCRIPTION = """Pajek Tools"""
+
+import sys, os, time
+from pathlib import Path
+from datetime import datetime
+from timeit import default_timer as timer
+try:
+    from humanfriendly import format_timespan
+except ImportError:
+    def format_timespan(seconds):
+        return "{:.2f} seconds".format(seconds)
+
+import logging
+logging.basicConfig(format='%(asctime)s %(name)s.%(lineno)d %(levelname)s : %(message)s',
+        datefmt="%H:%M:%S",
+        level=logging.INFO)
+# logger = logging.getLogger(__name__)
+logger = logging.getLogger('__main__').getChild(__name__)
+
+def main(args):
+    pass
+
+if __name__ == "__main__":
+    total_start = timer()
+    logger.info(" ".join(sys.argv))
+    logger.info( '{:%Y-%m-%d %H:%M:%S}'.format(datetime.now()) )
+    import argparse
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument("--debug", action='store_true', help="output debugging info")
+    global args
+    args = parser.parse_args()
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+        logger.debug('debug mode is on')
+    else:
+        logger.setLevel(logging.INFO)
+    main(args)
+    total_end = timer()
+    logger.info('all finished. total time: {}'.format(format_timespan(total_end-total_start)))
